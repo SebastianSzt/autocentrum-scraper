7b2e5ce945434c36349ac96e2a08fb8b17786c31
SebastianSzt <sebastiansztankowski@gmail.com>
2024-08-14 13:24:35 +0200

    Add error logging to main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,10 @@
 require_relative 'lib/links_scraper'
 require 'fileutils'
 require 'time'
+require 'logger'
+
+logger = Logger.new('error.log')
+logger.level = Logger::DEBUG
 
 def fetch_links(links_url, output_file)
   puts "Checking if links are cached in #{output_file}."
@@ -22,13 +26,30 @@ def fetch_links(links_url, output_file)
   links
 end
 
-links_url = 'https://www.autocentrum.pl/sitemap/daneTechniczne.xml'
-output_links_file = 'links.txt'
+begin
+  links_url = 'https://www.autocentrum.pl/sitemap/daneTechniczne.xml'
+  output_links_file = 'links.txt'
 
-puts "Fetching links..."
-links = fetch_links(links_url, output_links_file)
+  puts 'Fetching links...'
+  links = fetch_links(links_url, output_links_file)
 
-puts "Number of links: #{links.size}"
+  puts "Number of links: #{links.size}"
 
-puts "First 4 links:"
-links.first(4).each { |link| puts link }
+  puts 'First 4 links:'
+  links.first(4).each { |link| puts link }
+rescue IOError => e
+  logger.error("IOError occurred: #{e.message}")
+  logger.error(e.backtrace.join("\n"))
+rescue ArgumentError => e
+  logger.error("ArgumentError occurred: #{e.message}")
+  logger.error(e.backtrace.join("\n"))
+rescue RuntimeError => e
+  logger.error("RuntimeError occurred: #{e.message}")
+  logger.error(e.backtrace.join("\n"))
+rescue StandardError => e
+  logger.error("An error occurred: #{e.message}")
+  logger.error(e.backtrace.join("\n"))
+rescue Exception => e
+  logger.fatal("An unexpected error occurred: #{e.message}")
+  logger.fatal(e.backtrace.join("\n"))
+end
